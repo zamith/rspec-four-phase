2cb2511c0951e33c98231e050cde5c87e86b638a
Zamith <zamith.28@gmail.com>
2013-04-01 19:25:27 +0100

    Starts gem

---
+++ lib/rspec/four_phase.rb
@@ -0,0 +1,7 @@
+require "rspec/four_phase/version"
+
+module Rspec
+  module FourPhase
+    # Your code goes here...
+  end
+end
---
+++ lib/rspec/four_phase/version.rb
@@ -0,0 +1,5 @@
+module Rspec
+  module FourPhase
+    VERSION = "0.0.1"
+  end
+end
